7247f6239ff5d44f3c17218c79ad01d4d087fa7a
Desir10101000Dj <1172556452@qq.com>
2020-05-22 00:17:45 +0800

    “睡觉了”

--- gitstudy/main.c
+++ gitstudy/main.c
@@ -4,29 +4,12 @@
 int main(int argc, char *argv[])
 {
 	
-	/*
-		printf("main函数收到了%d个参数\n",argc);//第一个参数默认是可执行文件本身，第二个参数开始是接受到的参数
-		int i;
-		for( i = 0; i < argc; i++ ){
-			printf("%s ",argv[i]);    
-		}
-		printf("\n");
-	*/
+
 
 	if( argc == 3 ){
     		
-        //打印接受到的参数
-        /*
-			int i;
-			printf("收到的外部参数：");
-			for( i = 1; i < argc; i++ ){
-				printf("%s ",argv[i]);    
-			}
-			printf("\n");
-        */
-        
-        //尝试读取文件的内容
-        char data;
+     
+    
         FILE *fp = fopen(argv[2],"r");
         
         if(!fp){
@@ -46,7 +29,7 @@ int main(int argc, char *argv[])
                 temp = fgetc(fp);
                 num++;
 			}
-            printf("该文本文件的字符数为：%d\n",num);
+            printf("字符数：%d\n",num);
             
         }
         else if( !strcmp(argv[1],"-w") ){//单词数
@@ -63,35 +46,19 @@ int main(int argc, char *argv[])
                 else
                     a =fgetc(fp);
             }
-            printf("该文本文件的单词数为：%d\n",w);
+            printf("单词数：%d\n",w);
             
         }
-        else if( !strcmp(argv[1],"-l") ){//行数
-			char temp;
-			int num = 1;
-            temp = fgetc(fp);
-			while( temp != EOF ){
-				
-				//putchar(temp);
-                if(temp == '\n'){//字符需要用单引号扩住
-					num++;
-                }
-                temp = fgetc(fp);
-                
-                
-			}
-            printf("该文本文件的行数为：%d\n",num);
-	
-        }
+       
         else{
-			printf("错误：参数超出范围！\n");
+			printf("错误：参数超出范围！必须是-w 或者 -c\n");
             printf("允许参数：-c，-w，-l\n");
         }
         int n=fclose(fp);
     }
     else{
-		printf("错误：运行参数必须要3个\n");
-		printf("用法：wc.exe [parameter] [file_name]\n");
+		printf("错误：运行参数必须要3个!\n");
+		printf("用法：wc.exe [parameter] [file_name]\n 见实验报告");
     }
 	system("pause");
 	return 0;
